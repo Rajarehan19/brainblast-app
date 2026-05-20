8f35cadb2319843fe0b5d3902b14ce0fdebb9d21
Rajarehan19 <132757275+Rajarehan19@users.noreply.github.com>
2026-05-20 11:55:35 +0500

    Add MainActivity to load HTML from assets

---
+++ app/src/main/assets/app/src/main/java/com/brainblast/quiz/app/src/main/java/com/brainblast/quiz/MainActivity.java
@@ -0,0 +1,33 @@
+package com.brainblast.quiz;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends Activity {
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        webView = new WebView(this);
+        setContentView(webView);
+
+        WebSettings settings = webView.getSettings();
+        settings.setJavaScriptEnabled(true);
+        settings.setDomStorageEnabled(true);
+        settings.setAllowFileAccessFromFileURLs(true);
+        settings.setMixedContentMode(WebSettings.MIXED_CONTENT_ALWAYS_ALLOW);
+
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("file:///android_asset/index.html");
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (webView.canGoBack()) webView.goBack();
+        else super.onBackPressed();
+    }
+}
